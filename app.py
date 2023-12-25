0efc9e3d5bbecf6936f51f2bc6df6d7776e363c8
Arthur Moraes <arthursilva201413@gmail.com>
2023-12-25 19:57:05 -0300

    commit

--- app.py
+++ app.py
@@ -53,8 +53,8 @@ def like_instagram():
     senha = 'senha'
     pagina_curtir = 'https://www.instagram.com/devaprender/'
     driver.get(instagram)
-
  
+  
     #logar na conta
     digitar_email = driver.find_element(By.XPATH, "//input[@aria-label='Phone number, username, or email']")
     digitar_email.send_keys(email)
